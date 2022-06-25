c04013767b48169eb7c61942942cf8cdd365f754
tmngtomo <90687765+tmngtomo@users.noreply.github.com>
2022-06-25 22:13:36 +0900

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+export const Header = () =>
+{
+  const componentsName = () => 'Header';
+  return(
+    <div className="test-wrapper">
+      このコンポーネントの名前は{componentsName()}です。
+    </div>
+
+
+
+  );
+}
