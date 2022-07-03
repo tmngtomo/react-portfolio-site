ab8a07dee98e86b16a4d40e0b0dfd084905a5f5a
tomomi <90687765+tmngtomo@users.noreply.github.com>
2022-07-03 22:04:53 +0900

    skillreducers created

--- src/reducers/skillReducer.js
+++ src/reducers/skillReducer.js
@@ -6,3 +6,27 @@ export const actionTypes = {
   success: 'FETCH_SUCCESS',
   error: 'FETCH_ERROR'
 };
+
+export const initialState = {
+  languageList : [],
+  requestState: requestStates.idle,
+}
+
+// 省略
+export const skillReducer = (
+  state, action
+) => {
+  switch(action.type) {
+    case actionTypes.initial: {
+    }
+    case actionTypes.fetch: {
+    }
+    case actionTypes.success: {
+    }
+    case actionTypes.error: {
+    }
+    default: {
+      throw new Error();
+    }
+  }
+};
